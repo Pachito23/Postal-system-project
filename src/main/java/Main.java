6727e9a4ac22cedafb58e8b430dbab48d3b4a9cc
Pachito23 <pasquale.vivaldo@student.upt.ro>
2022-05-23 22:55:06 +0300

    Automated testing part2 (finalized)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
-import javafx.stage.Stage;
-
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
 
     }
 }
